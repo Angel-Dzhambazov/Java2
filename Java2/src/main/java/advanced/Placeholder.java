ab7323f4fddfc381c718aa2310cf7f5017ac19e7
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2019-03-06 19:13:15 +0200

    Uploading 2019 exam materials

---
+++ Java2/src/main/java/advanced/Placeholder.java
@@ -0,0 +1,17 @@
+/*
+ * Placeholder.java
+ *
+ * created at 2019-03-06 by a.dzhambazov <YOURMAILADDRESS>
+ * 
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+package advanced;
+
+
+public class Placeholder
+{
+
+}
+
+
+
---
+++ Java2/src/main/java/advanced/exams/y2019_Feb_17/Socks.java
@@ -0,0 +1,13 @@
+package advanced.exams.y2019_Feb_17;
+
+
+public class Socks
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi");
+	}
+}
+
+
+
